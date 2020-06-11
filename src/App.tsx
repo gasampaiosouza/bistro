fdf51180cd099392677b6c020101982b79a0a1a9
gasampaiosouza <gasampaio.souza@gmail.com>
2020-06-11 00:32:27 -0300

    add responsive header as default component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 
-import Box from './components/default/Box';
-import Input from './components/default/Input';
-import Button from './components/default/Button';
+import Header from './components/default/Header';
 
 function App() {
   return (
     <div className="container">
-      <Box
-        media={[10, 23, 30, 40, 50, 324, 429, 423, 2, 4]}
-        itemName="Hamburguer"
-        quantity={40}
-      />
-
-      <Input placeholder="something" />
-      <Button text="something" />
+      <Header />
     </div>
   );
 }
